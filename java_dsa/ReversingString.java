4851ca0f1fb4200785c6ec6443ed67e162855b4a
gnsaddy <aditya.x510@gmail.com>
2020-10-10 23:06:17 +0530

    update

--- java_dsa/ReversingString.java
+++ java_dsa/ReversingString.java
@@ -9,29 +9,29 @@ class ReversingString {
         System.out.println("string: "+ str);
         System.out.println("string length: "+ str.length());
 
-        // for(int i=str.length()-1;i>=0;i--){
+        for(int i=str.length()-1;i>=0;i--){
 
-        //     System.out.print(str.charAt(i) + "");
-        // }
-
-        // System.out.println("\n--------------");
-        // System.out.println("string: "+ (str.reverse().toString()));
-
-
-        String[] strArray = str.toString().split(" ");
-        // System.out.println(strArray.length);
-
-        for(int i=0;i<strArray.length;i++){
-            StringBuilder sb=new StringBuilder();  
+            System.out.print(str.charAt(i) + "");
         }
+
+        System.out.println("\n--------------");
+        System.out.println("string: "+ (str.reverse().toString()));
+
+
+        // String[] strArray = str.toString().split(" ");
+        // // System.out.println(strArray.length);
+
+        // for(int i=0;i<strArray.length;i++){
+        //     StringBuilder sb=new StringBuilder();  
+        // }
             
-        for(int i=0; i<strArray.length; i++){ 
-            char[] s1 = strArray[i].toCharArray(); 
-            for (int j = s1.length-1; j>=0; j--){
-                System.out.print(s1[j]);
-            }
-                System.out.print(" ");
-            }
+        // for(int i=0; i<strArray.length; i++){ 
+        //     char[] s1 = strArray[i].toCharArray(); 
+        //     for (int j = s1.length-1; j>=0; j--){
+        //         System.out.print(s1[j]);
+        //     }
+        //         System.out.print(" ");
+        //     }
             System.out.println(); 
     }
 }
